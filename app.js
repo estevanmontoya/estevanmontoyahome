b4eef44f3b3e040e8b2bc8d7ad3e13d16038afd5
estevanmontoya <montoya.estevan@gmail.com>
2017-03-07 11:26:45 -0700

    Added Pug, views directory, and basic end points for homes page

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
-var express = require('express');
+const express = require('express');
+const pug = require('pug');
 
-var app = express();
+const app = express();
 
+// SET Pug views
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+
+// USE public directory
+app.use(express.static(__dirname + '/public'));
+
+// End point views
 app.get('/', function(req, res) {
-  res.send('Mic check');
+  res.render('index');
+});
+
+app.get('/about', function(req, res) {
+  res.render('about');
+});
+
+app.get('/resources', function(req, res) {
+  res.render('resources');
+})
+
+app.listen(3000, function(){
+  console.log('Site is running at http://localhost:3000');
 });
